0bcc89ad48262c318011651808b17fe6888a8b1c
archibate <1931127624@qq.com>
2026-02-04 16:04:04 +0800

    upgrade fish plugins

--- setup.fish
+++ setup.fish
@@ -1,10 +1,10 @@
 #!/usr/bin/env fish
 
+set -l fisher_plugins (cat $__fish_config_dir/fish_plugins)
+
 if not command -sq fisher
     curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 end
-
-set -l fisher_plugins jethrokuan/z acomagu/fish-async-prompt jorgebucaran/autopair.fish franciscolourenco/done mattmc3/magic-enter.fish jorgebucaran/fisher budimanjojo/tmux.fish pure-fish/pure patrickf1/fzf.fish jorgebucaran/replay.fish meaningful-ooo/sponge paldepind/projectdo
 
 for plugin in $fisher_plugins
     fisher install $plugin
@@ -13,7 +13,9 @@ end
 set -U fish_tmux_autostart false
 set -U fish_tmux_autoname_session true
 set -U fish_tmux_no_alias true
-set -U pure_reverse_prompt_symbol_in_vimode true
-set -U pure_enable_single_line_prompt true
-set -U pure_separate_prompt_on_error true
-set -U pure_show_system_time false
+
+for x in local atuin cargo npm-global opencode
+    if test -d ~/.$x/bin
+        fish_add_path -U ~/.$x/bin/
+    end
+end
